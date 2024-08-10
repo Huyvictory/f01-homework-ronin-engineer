143e109caced12da13b58ec6201bcda8b2422df3
Huyvictory <vominhhuy008@gmail.com>
2024-08-10 23:08:48 +0700

    Add assignment 3 from the BE Fundamentals course

--- Main.java
+++ Main.java
@@ -2,7 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+        // palindrome string check
+        String palindrome = "abba";
+
+
+        int j = palindrome.length() - 1;
+
+        for (int i = 0; i < palindrome.length(); i++) {
+            if (palindrome.charAt(i) != palindrome.charAt(j)) {
+                System.out.println("Not a palindrome");
+                return;
+            }
+
+            if (i >= j) {
+                break;
+            }
+            j--;
+        }
+
+        System.out.println("Palindrome string");
+
+    }
 }
