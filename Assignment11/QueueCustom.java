058bf993a521de7b6bca9e0d00325540019f1e8d
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 00:35:41 +0700

    Add new assignment 11

---
+++ Assignment11/QueueCustom.java
@@ -0,0 +1,54 @@
+package Assignment11;
+
+import java.util.ArrayList;
+
+public class QueueCustom {
+    private ArrayList<Integer> queueArray;
+
+    public void init() {
+        System.out.println("Initializing queue");
+        queueArray = new ArrayList<>();
+    }
+
+    public int size() {
+        return queueArray.size();
+    }
+
+    public int isEmpty() {
+        return queueArray.isEmpty() ? -1 : 0;
+    }
+
+    public void enQueue(int value) {
+        if (value < 1000000) {
+            queueArray.add(value);
+            System.out.println("Queue array after adding " + value + " is " + queueArray);
+        }
+    }
+
+    public void deQueue() {
+        if (isEmpty() == -1) {
+            System.out.println("Queue is empty");
+        } else {
+            int removedValue = queueArray.removeFirst();
+            System.out.println("Removed value is " + removedValue);
+            System.out.println("Queue array after removing " + removedValue + " is " + queueArray);
+        }
+    }
+
+    public void peek() {
+        if (isEmpty() == -1) {
+            System.out.println("-1");
+        } else {
+            System.out.println("First element is " + queueArray.getFirst());
+        }
+    }
+
+    public void tail() {
+        if (isEmpty() == -1) {
+            System.out.println("-1");
+        } else {
+            System.out.println("Last element is " + queueArray.getLast());
+        }
+    }
+
+}
