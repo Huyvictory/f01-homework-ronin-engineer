418dfe0f45568c834a800dd2a805ef54d7617e40
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 10:57:27 +0700

    Update assignment 02 of feature/01-Java-Introduction

--- Assignment_1.java
+++ Assignment_1.java
@@ -10,5 +10,15 @@ public class Assignment_1 {
 
         System.out.println();
 
+        // Bai 2
+        System.out.println("*****************************************");
+        System.out.println("    J    A   V     V  A");
+        System.out.println("    J   A A   V   V  A A");
+        System.out.println("J   J  AAAAA   V V  AAAAA");
+        System.out.println(" J J  A     A   V  A     A");
+        System.out.println("******************************************");
+
+        System.out.println();
+
     }
 }
