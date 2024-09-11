192ef81c57091d761e4fb8b687897e3c965f49c8
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 11:46:07 +0700

    Update assignments 6

--- Assignment_2.java
+++ Assignment_2.java
@@ -39,6 +39,13 @@ public class Assignment_2 {
         System.out.println("******************************************");
 
         System.out.println();
+
+        // Bai 6
+        System.out.println("******************************************");
+        Bai6();
+        System.out.println("******************************************");
+
+        System.out.println();
     }
 
     public static void Bai1() {
@@ -146,6 +153,34 @@ public class Assignment_2 {
         }
 
         System.out.println("Số thập phân: " + soThapPhanTuMaNhiPhan);
+    }
+
+    public static String chuyenVeLaMa(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(symbols[i]);
+            }
+        }
+
+        return roman.toString();
+    }
+
+    public static void Bai6() {
+        Scanner scanner6 = new Scanner(System.in);
+        int soNguyenDuong;
+        do {
+            System.out.println("Nhập số nguyên dương nhỏ hơn 1000");
+            soNguyenDuong = scanner6.nextInt();
+        }
+        while (soNguyenDuong < 0 || soNguyenDuong > 1000);
+
+        System.out.println("Sô la mã của số nguyên dương " + soNguyenDuong + " là " + chuyenVeLaMa(soNguyenDuong));
     }
 
 }
