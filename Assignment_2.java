46c2762c5cf1d9e2c3a97e038b4d50e57fabca45
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 11:34:18 +0700

    Add new branch feature/02-Variables-and-Data-type

---
+++ Assignment_2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Assignment_2 {
+    public static void main(String[] args) {
+    }
+
+}
+
