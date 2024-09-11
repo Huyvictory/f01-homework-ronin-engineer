2daf10f4092d1b531434d994ee3c1be9d2df6b49
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 12:08:18 +0700

    Update utils package

--- Utils/InputUtils.java
+++ Utils/InputUtils.java
@@ -20,4 +20,19 @@ public class InputUtils {
 
         return intVariable;
     }
+
+    public static double InputValidDoubleNumber(Scanner scanner, String nameVariable) {
+        double doubleVariable;
+        System.out.println("Enter a double " + nameVariable);
+        do {
+            String userInput = scanner.next();
+            try {
+                doubleVariable = Double.parseDouble(userInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input please enter a valid double number");
+            }
+        } while (true);
+        return doubleVariable;
+    }
 }
