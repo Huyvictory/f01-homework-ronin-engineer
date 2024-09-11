3b46e961f8e5679d3230e5f5829bebbea5c38539
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 10:54:29 +0700

    Update assignment 01 of feature/01-Java-Introduction

---
+++ Assignment_1.java
@@ -0,0 +1,14 @@
+public class Assignment_1 {
+    public static void main(String[] args) {
+
+        // Bai 1
+        System.out.println("*****************************************");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+        System.out.println("******************************************");
+
+        System.out.println();
+
+    }
+}
