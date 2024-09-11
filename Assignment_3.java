963a637af904da32a983cc904975d83d56ed9a4b
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 11:53:08 +0700

    Update assignments 1

---
+++ Assignment_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Assignment_3 {
+    public static void main(String[] args) {
+        // Bai 1
+        System.out.println("*****************************************");
+        Bai1();
+        System.out.println("******************************************");
+        System.out.println();
+
+    }
+
+    private static void Bai1() {
+        String stringVariable = "Hello World";
+        System.out.println(stringVariable);
+    }
+}
