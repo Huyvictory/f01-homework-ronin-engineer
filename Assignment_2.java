b87586a4cdcaf15241019c0d41ffdf5eb933dc29
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 11:36:20 +0700

    Update assignments 1

--- Assignment_2.java
+++ Assignment_2.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 
 public class Assignment_2 {
     public static void main(String[] args) {
+        // Bai 1
+        System.out.println("*****************************************");
+        Bai1();
+        System.out.println("******************************************");
+
+        System.out.println();
+    }
+
+    public static void Bai1() {
+        double inchInput;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Nhập so inch");
+            inchInput = scanner.nextDouble();
+
+        } while (inchInput <= 0);
+
+        System.out.println("Số inch: " + inchInput + "được chuyển về " + inchInput * 2.54 + " cm");
     }
 
 }
