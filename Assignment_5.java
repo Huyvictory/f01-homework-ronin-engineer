31bad4596d7498dc04fea83de114b12d5ce2b33a
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 12:27:06 +0700

    Update assignment 2

--- Assignment_5.java
+++ Assignment_5.java
@@ -1,3 +1,5 @@
+import Utils.InputUtils;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +14,13 @@ public class Assignment_5 {
         Bai1();
         System.out.println("*****************************************");
         System.out.println();
+
+        // Bai 2
+        System.out.println("*****************************************");
+        int foundedIndex = Bai2();
+        System.out.printf("The founded index is %d\n", foundedIndex);
+        System.out.println("*****************************************");
+        System.out.println();
     }
 
     private static void Bai1() {
@@ -23,6 +32,21 @@ public class Assignment_5 {
         }
 
         System.out.println("The sum of the array is " + sum);
+    }
+
+    private static int Bai2() {
+        Scanner scanner = new Scanner(System.in);
+        int[] arrayNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int n = InputUtils.InputValidIntegerNumber(scanner, "n");
+
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            if (arrayNumbers[i] == n) {
+                return i;
+            }
+        }
+        return -1;
+
     }
 
 }
