b956392fc7443e480f69b9661ae974f548c16ef0
Huyvictory <vominhhuy008@gmail.com>
2024-09-11 11:54:13 +0700

    Update assignments 2

--- Assignment_3.java
+++ Assignment_3.java
@@ -8,10 +8,59 @@ public class Assignment_3 {
         System.out.println("******************************************");
         System.out.println();
 
+        // Bai 2
+        System.out.println("*****************************************");
+        Bai2();
+        System.out.println("******************************************");
+        System.out.println();
+
     }
 
     private static void Bai1() {
         String stringVariable = "Hello World";
         System.out.println(stringVariable);
+    }
+
+    public static int InputValidIntegerNumber(Scanner scanner, String nameVariable) {
+        int intVariable;
+        System.out.println("Enter an integer " + nameVariable);
+        do {
+            String userInput = scanner.next();
+
+            try {
+                intVariable = Integer.parseInt(userInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input please enter a valid integer number");
+            }
+
+        } while (true);
+
+        return intVariable;
+    }
+
+    private static void Bai2() {
+        int intVariable;
+
+        Scanner scanner = new Scanner(System.in);
+        intVariable = InputValidIntegerNumber(scanner, "number");
+
+        System.out.println("You entered: " + intVariable);
+
+        // For positive numbers
+        if (intVariable > 0 && intVariable % 2 == 0) {
+            System.out.println("The number is positive and even");
+        }
+        if (intVariable > 0 && intVariable % 2 != 0) {
+            System.out.println("The number is positive and odd");
+        }
+
+        // For negative numbers
+        if (intVariable < 0 && intVariable % 2 == 0) {
+            System.out.println("The number is negative and even");
+        }
+        if (intVariable < 0 && intVariable % 2 != 0) {
+            System.out.println("The number is negative and odd");
+        }
     }
 }
